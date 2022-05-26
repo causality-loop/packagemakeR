db38b3a76fde3d98ecc2940d8ed395eb4400a0a6
markbojack <hoggel_tabke@slmail.me>
2022-05-26 13:34:34 -0400

    add from_date

--- inst/extdata/dictionary.R
+++ inst/extdata/dictionary.R
@@ -3,6 +3,7 @@
 #' @param email character, your email address
 #' @param first_and_last_names character, your first and last names, separated by a space
 #' @param fork character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
+#' @param from_date character, representing a date in YYYY-MM-DD format, it indicates the date which fills in missing date values
 #' @param github_username character, your username on GitHub
 #' @param has_downloads character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
 #' @param has_issues character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
@@ -13,7 +14,7 @@
 #' @param package_description character, description of the package which will be written in `DESCRIPTION`
 #' @param package_description character, the package description which will be on GitHub
 #' @param package_directory character, the full path of the directory containing the package
+#' @param package_directory character, the full path of the package directory
 #' @param private character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'false'
 #' @param repo_name character, the preferred CRAN repo used to update R packages, Default: https://cran.seoul.go.kr/
 #' @param source_directory character, the full path of the source directory (the directory containing .R files in which work has been done to develop the package)
-#' @param package_directory character, the full path of the package directory
