29dcdbc941785d4cc2a6fc4be2c9655fffa89c23
markbojack <hoggel_tabke@slmail.me>
2022-05-27 10:08:01 -0400

    minor edits

--- inst/extdata/dictionary.R
+++ inst/extdata/dictionary.R
@@ -1,9 +1,11 @@
 #' @param \dots additional parameters passed to \code{somePackage}
+#' @param asset_info a character vector of asset symbols or a list of character vectors containing symbol and date info (see *Note*)
 #' @param dictionary_file character, the location of the dictionary file (please indicate full file path)
 #' @param email character, your email address
 #' @param first_and_last_names character, your first and last names, separated by a space
 #' @param fork character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
-#' @param from_date character, representing a date in YYYY-MM-DD format, it indicates the date which fills in missing date values
+#' @param from_date a character (representing a date in YYYY-MM-DD format) which fills in missing values for the start of each date range
+#' @param from_date a character (representing a date in YYYY-MM-DD format) which is passed to `quantmod::getSymbols()`
 #' @param github_username character, your username on GitHub
 #' @param has_downloads character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
 #' @param has_issues character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
@@ -13,7 +15,6 @@
 #' @param homepage character, either 'null' or the project URL which will be shown on GitHub as the project homepage, Default: 'null'
 #' @param package_description character, description of the package which will be written in `DESCRIPTION`
 #' @param package_description character, the package description which will be on GitHub
-#' @param package_directory character, the full path of the directory containing the package
 #' @param package_directory character, the full path of the package directory
 #' @param private character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'false'
 #' @param repo_name character, the preferred CRAN repo used to update R packages, Default: https://cran.seoul.go.kr/
