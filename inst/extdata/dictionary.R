ebf54db10ace0de1c4c3c19638ac9784a5b7ba63
markbojack <hoggel_tabke@slmail.me>
2022-06-22 16:56:42 -0400

    add defs

--- inst/extdata/dictionary.R
+++ inst/extdata/dictionary.R
@@ -1,7 +1,9 @@
 #' @param \dots additional parameters passed to \code{somePackage}
 #' @param asset_info a character vector of asset symbols or a list of character vectors containing symbol and date info (see *Note*)
+#' @param deploy boolean, TRUE if the model should be deployed in a live trading environment, FALSE if it's just being used for testing/research, Default: TRUE
 #' @param dictionary_file character, the location of the dictionary file (please indicate full file path)
 #' @param email character, your email address
+#' @param file_name character, the file name relative to the work directory or, if none, the full path to the file
 #' @param first_and_last_names character, your first and last names, separated by a space
 #' @param fork character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'true'
 #' @param from_date a character (representing a date in YYYY-MM-DD format) which fills in missing values for the start of each date range
@@ -13,6 +15,7 @@
 #' @param has_projects character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'false'
 #' @param has_wiki character, a boolean 'true' or 'false' value (but inputted as a character in lowercase letters) which indicates a GitHub repo option, Default: 'false'
 #' @param homepage character, either 'null' or the project URL which will be shown on GitHub as the project homepage, Default: 'null'
+#' @param model_units, how many units to allocate to this model
 #' @param package_description character, description of the package which will be written in `DESCRIPTION`
 #' @param package_description character, the package description which will be on GitHub
 #' @param package_directory character, the full path of the package directory
